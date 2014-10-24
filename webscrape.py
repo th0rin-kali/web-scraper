d99aa42ca6aafac4eb8ae028fa9ad1c6fc41bd2c
surf666 <sushil.g.shenoy@gmail.com>
2014-10-24 18:57:07 +0530

    still under progress

--- webscrape.py
+++ webscrape.py
@@ -3,10 +3,11 @@ import sys, getopt
 
 def main (argv):
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "p:t:h:o:g",['help'])
+        opts, args = getopt.getopt(sys.argv[1:], "p:t:w:o:g",['help'])
     except getopt.GetoptError as err:
         print str(err)
         usage()
         sys.exit(2)
+
 
 
